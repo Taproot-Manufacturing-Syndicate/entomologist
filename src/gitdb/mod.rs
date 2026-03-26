6fc3c2a847c6a345d844af16622d84388f852ea2
Sebastian Kuzminsky <seb@highlab.com>
2026-03-25 22:42:34 -0600

    move `ensure_branch_exists()` from crate::git to crate::gitdb
    And make it private to gitdb, no one else needs to care about it.

--- src/gitdb/mod.rs
+++ src/gitdb/mod.rs
@@ -27,7 +27,15 @@ pub struct GitDbMut {
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error(transparent)]
-    Git(#[from] crate::git::GitError),
+    StdIoError(#[from] std::io::Error),
+
+    #[error("Error from git:\nstdout: {stdout}\nstderr: {stderr}")]
+    Git { stdout: String, stderr: String },
+
+    // The GitError is a leftover, should go away when the
+    // entomologist::git module gets incorporated into entomologist::gitdb.
+    #[error(transparent)]
+    GitError(#[from] crate::git::GitError),
 
     #[error(transparent)]
     Worktree(#[from] worktree::Error),
@@ -37,7 +45,7 @@ impl GitDb {
     /// Check out a git ref into an ephemeral worktree, in detached
     /// head mode..
     pub fn get(git_ref: &str) -> Result<GitDb, Error> {
-        crate::git::ensure_branch_exists(git_ref)?;
+        ensure_branch_exists(git_ref)?;
         Ok(GitDb {
             worktree: worktree::Worktree::new_detached(git_ref)?,
         })
@@ -53,7 +61,7 @@ impl GitDbMut {
     /// Check out a git ref into an ephemeral worktree, in normal (named
     /// branch) mode.
     pub fn get(git_ref: &str) -> Result<GitDbMut, Error> {
-        crate::git::ensure_branch_exists(git_ref)?;
+        ensure_branch_exists(git_ref)?;
         Ok(GitDbMut {
             worktree: worktree::Worktree::new(git_ref)?,
         })
@@ -63,4 +71,47 @@ impl GitDbMut {
     pub fn path(&self) -> std::path::PathBuf {
         self.worktree.path().into()
     }
+}
+
+fn ensure_branch_exists(branch: &str) -> Result<(), Error> {
+    // Check for a local branch with the specified name.
+    if crate::git::git_branch_exists(&format!("refs/heads/{branch}"))? {
+        return Ok(());
+    }
+
+    // Check for *any* branch with the specified name, even remote.
+    let result = std::process::Command::new("git")
+        .args(["show-ref", branch])
+        .output()?;
+    match result.status.success() {
+        true => {
+            // Some remote has this branch, make a local branch from
+            // the first one found.
+            let output = String::from_utf8_lossy(&result.stdout);
+            let line = output.split('\n').next().ok_or(Error::Git {
+                stdout: String::from_utf8_lossy(&result.stdout).into(),
+                stderr: String::from_utf8_lossy(&result.stderr).into(),
+            })?;
+            let remote_branch = line.split_whitespace().last().ok_or(Error::Git {
+                stdout: String::from_utf8_lossy(&result.stdout).into(),
+                stderr: String::from_utf8_lossy(&result.stderr).into(),
+            })?;
+
+            let result = std::process::Command::new("git")
+                .args(["branch", branch, remote_branch])
+                .output()?;
+            if !result.status.success() {
+                return Err(Error::Git {
+                    stdout: String::from_utf8_lossy(&result.stdout).into(),
+                    stderr: String::from_utf8_lossy(&result.stderr).into(),
+                });
+            }
+        }
+        false => {
+            // No remote has this branch, make an empty one locally now.
+            crate::git::create_orphan_branch(branch)?;
+        }
+    }
+
+    Ok(())
 }
