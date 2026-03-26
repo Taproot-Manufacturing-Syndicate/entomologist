9a6b6474bcc8c14a031ca55464cf6ad40377f85c
Sebastian Kuzminsky <seb@highlab.com>
2026-03-25 22:37:39 -0600

    gitdb doc comments

--- src/gitdb/mod.rs
+++ src/gitdb/mod.rs
@@ -1,10 +1,24 @@
+//! gitdb is a front end that lets you access a git branch by checking
+//! it out in a temporary worktree.
+//!
+//! This module is used internally by entomologist, the user generally
+//! doesn't need to care about it or use it directly.
+
 pub mod worktree;
 
+/// GitDb checks out a git ref in detached head mode, so any changes
+/// made to the worktree can **not** be committed back to the ref. This
+/// makes the GitDb effectively immutable, in the sense that there's no
+/// way to make lasting changes to the git ref.
 #[derive(Debug)]
 pub struct GitDb {
     worktree: crate::gitdb::worktree::Worktree,
 }
 
+/// GitDbMut checks out a git ref in normal (named branch) mode, so
+/// any changes made to the worktree **can** be committed back to the
+/// ref. This makes the GitDbMut mutable, in the sense that it can add
+/// commits to the git ref.
 #[derive(Debug)]
 pub struct GitDbMut {
     worktree: crate::gitdb::worktree::Worktree,
@@ -20,7 +34,8 @@ pub enum Error {
 }
 
 impl GitDb {
-    /// Check out a git ref into an ephemeral worktree.
+    /// Check out a git ref into an ephemeral worktree, in detached
+    /// head mode..
     pub fn get(git_ref: &str) -> Result<GitDb, Error> {
         crate::git::ensure_branch_exists(git_ref)?;
         Ok(GitDb {
@@ -28,13 +43,15 @@ impl GitDb {
         })
     }
 
+    /// Get the path of the worktree.
     pub fn path(&self) -> std::path::PathBuf {
         self.worktree.path().into()
     }
 }
 
 impl GitDbMut {
-    /// Check out a git ref into an ephemeral worktree.
+    /// Check out a git ref into an ephemeral worktree, in normal (named
+    /// branch) mode.
     pub fn get(git_ref: &str) -> Result<GitDbMut, Error> {
         crate::git::ensure_branch_exists(git_ref)?;
         Ok(GitDbMut {
@@ -42,6 +59,7 @@ impl GitDbMut {
         })
     }
 
+    /// Get the path of the worktree.
     pub fn path(&self) -> std::path::PathBuf {
         self.worktree.path().into()
     }
